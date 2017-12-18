51cdca6a5b4f945f9733be2824d2196fdeedffb6
Davey Contreras <dcontreras52@gmail.com>
2017-12-18 08:08:30 -0500

    Ignore database.php file to avoid tracking environment configuration and provide a default.

---
+++ protected/config/database-default.php
@@ -0,0 +1,16 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+//	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+
+    'connectionString' => 'mysql:host=yourhost;dbname=nameofdatabase',
+    'emulatePrepare' => true,
+    'username' => 'username',
+    'password' => 'password',
+    'charset' => 'utf8',
+//    'tablePrefix' => 'tbl_',
+
+
+);
