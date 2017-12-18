786e24af9fcf66ced7b48f0a3c1360a445bdf03d
Davey Contreras <dcontreras52@gmail.com>
2017-12-18 07:45:24 -0500

    Define actions that can be executed in cart.

---
+++ protected/controllers/CartController.php
@@ -0,0 +1,72 @@
+<?php
+
+class CartController extends Controller {
+
+    /**
+     * Lists all cart items.
+     */
+    public function actionIndex() {
+        $cart = new ShoppingCart;
+        $cart->update();
+        $this->render('index', array(
+                'cart' => $cart)
+        );
+    }
+
+    /**
+     * Lists all cart items.
+     */
+    public function actionCheckout() {
+        $order = new Order;
+        $cart = new ShoppingCart;
+        $cart->update();
+        $this->render('//order/create', array(
+            'cart' => $cart,
+            'model' => $order
+            )
+        );
+    }
+
+    /**
+     * Remove product from cart.
+     */
+    public function actionRemove($product) {
+        $cart = new ShoppingCart;
+        if (isset($product)) {
+            $cart->remove($product);
+        }
+
+        $this->render('index', array(
+                'cart' => $cart
+            )
+        );
+    }
+
+    // -----------------------------------------------------------
+    // Uncomment the following methods and override them if needed
+    /*
+    public function filters()
+    {
+        // return the filter configuration for this controller, e.g.:
+        return array(
+            'inlineFilterName',
+            array(
+                'class'=>'path.to.FilterClass',
+                'propertyName'=>'propertyValue',
+            ),
+        );
+    }
+
+    public function actions()
+    {
+        // return external action classes, e.g.:
+        return array(
+            'action1'=>'path.to.ActionClass',
+            'action2'=>array(
+                'class'=>'path.to.AnotherActionClass',
+                'propertyName'=>'propertyValue',
+            ),
+        );
+    }
+    */
+}
