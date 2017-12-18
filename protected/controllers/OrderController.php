f45e385c6f1c893287d8d05b81e3073f3599cc2b
Davey Contreras <dcontreras52@gmail.com>
2017-12-18 07:46:46 -0500

    [Order] - Define actions that can be executed for an order.

---
+++ protected/controllers/OrderController.php
@@ -0,0 +1,181 @@
+<?php
+
+class OrderController extends Controller {
+    /**
+     * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+     * using two-column layout. See 'protected/views/layouts/column2.php'.
+     */
+    public $layout = '//layouts/column2';
+
+    /**
+     * @var CActiveRecord the currently loaded data model instance.
+     */
+    private $_model;
+
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow',  // allow all users to perform 'index' and 'view' actions
+                'actions' => array('create', 'view'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'actions' => array('update', 'index'),
+                'users' => array('@'),
+            ),
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions' => array('admin', 'delete'),
+                'users' => array('admin'),
+            ),
+            array('deny',  // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+    /**
+     * Displays a particular model.
+     */
+    public function actionView() {
+        $this->render('view', array(
+            'model' => $this->loadModel(),
+        ));
+    }
+
+    /**
+     * Creates a new model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionCreate() {
+        $model = new Order;
+        $cart = new ShoppingCart;
+        $cart->update();
+        $items = (object)$cart->items;
+
+        if (isset($_POST['Order'])) {
+            $order_attr = array(
+                'employee_id' => $_POST['Order']['employee_id'],
+                'customer_name' => $_POST['Order']['customer_name'],
+                'zipcode' => $_POST['Order']['zipcode'],
+                'qty' => $cart->qty,
+                'total' => $cart->total,
+                'status' => $model::PENDING_ORDER_STATUS,
+                'order_date' => time(),
+            );
+            echo json_encode($order_attr);
+
+            $model->attributes = $order_attr;
+            if ($model->save()) {
+                foreach ($items as $item) {
+                    $line_item = new LineItem;
+                    $line_item->order_id = $model->id;
+                    $line_item->product_id = $item['product']['id'];
+                    $line_item->qty = $item['qty'];
+                    $line_item->save();
+                }
+                unset($_SESSION['cart']);
+                Yii::app()->user->setFlash('OrderSubmitted', 'Thank You, we will begin processing your order!');
+                $this->redirect(array('view', 'id' => $model->id));
+            }
+        }
+    }
+
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     */
+    public function actionUpdate() {
+        $model = $this->loadModel();
+
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
+
+        if (isset($_POST['Order'])) {
+            $model->attributes = $_POST['Order'];
+            if ($model->save())
+                $this->redirect(array('view', 'id' => $model->id));
+        }
+
+        $this->render('update', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     */
+    public function actionDelete() {
+        if (Yii::app()->request->isPostRequest) {
+            // we only allow deletion via POST request
+            $this->loadModel()->delete();
+
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_GET['ajax']))
+                $this->redirect(array('index'));
+        } else
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex() {
+        $dataProvider = new CActiveDataProvider('Order');
+        $this->render('index', array(
+            'dataProvider' => $dataProvider,
+        ));
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin() {
+        $model = new Order('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['Order']))
+            $model->attributes = $_GET['Order'];
+
+        $this->render('admin', array(
+            'model' => $model,
+        ));
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     */
+    public function loadModel() {
+        if ($this->_model === null) {
+            if (isset($_GET['id']))
+                $this->_model = Order::model()->findbyPk($_GET['id']);
+            if ($this->_model === null)
+                throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $this->_model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param CModel the model to be validated
+     */
+    protected function performAjaxValidation($model) {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'order-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+}
