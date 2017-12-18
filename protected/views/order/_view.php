0050a9e7c945b06204bc95b63a809168d5279a2f
Davey Contreras <dcontreras52@gmail.com>
2017-12-18 07:47:50 -0500

    [Order] - Create views for the order CRUD functionalities.

---
+++ protected/views/order/_view.php
@@ -0,0 +1,38 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('employee_id')); ?>:</b>
+	<?php echo CHtml::encode($data->employee_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('customer_name')); ?>:</b>
+	<?php echo CHtml::encode($data->customer_name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('zipcode')); ?>:</b>
+	<?php echo CHtml::encode($data->zipcode); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('qty')); ?>:</b>
+	<?php echo CHtml::encode($data->qty); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('total')); ?>:</b>
+	<?php echo CHtml::encode($data->total); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
+	<?php echo CHtml::encode($data->status); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('order_date')); ?>:</b>
+	<?php echo CHtml::encode($data->order_date); ?>
+	<br />
+
+	*/ ?>
+
+</div>
---
+++ protected/views/order/create.php
@@ -0,0 +1,123 @@
+<?php
+$this->breadcrumbs=array(
+	'Orders'=>array('index'),
+	'Create',
+);
+
+$this->menu=array(
+	array('label'=>'List Order', 'url'=>array('index')),
+	array('label'=>'Manage Order', 'url'=>array('admin')),
+);
+?>
+<style>
+
+    table.table-view {
+        background: white;
+        border-collapse: collapse;
+        width: 100%;
+        margin: 0;
+    }
+
+    table.table-view tr.odd {
+        background: #E5F1F4;
+    }
+
+    table.table-view tr.even {
+        background: #F8F8F8;
+    }
+</style>
+<h1>Create Order</h1>
+
+<div class="form">
+
+    <?php $form=$this->beginWidget('CActiveForm', array(
+        'id'=>'order-form',
+        'action' => CHtml::normalizeUrl(array('order/create')),
+        'enableAjaxValidation'=>false,
+    )); ?>
+
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
+
+    <?php echo $form->errorSummary($model); ?>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'employee_id'); ?>
+        <?php echo $form->dropDownList($model, 'employee_id', Order::listItem('Employee')); ?>
+        <?php echo $form->error($model,'employee_id'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'customer_name'); ?>
+        <?php echo $form->textField($model,'customer_name',array('size'=>60,'maxlength'=>128)); ?>
+        <?php echo $form->error($model,'customer_name'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'zipcode'); ?>
+        <?php echo $form->textField($model,'zipcode',array('size'=>60,
+            'maxlength'=>128)); ?>
+        <?php echo $form->error($model,'zipcode'); ?>
+    </div>
+
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+
+</div><!-- form -->
+<hr>
+<h1>Cart Summary</h1>
+<div class="cart">
+    <table class="table-view">
+        <?php $i; ?>
+        <?php foreach ($cart->items as $item): ?>
+            <?php $i++; ?>
+            <tr class="<?php echo $i % 2 ? 'odd' : 'even'; ?>">
+                <td><?php echo $item['product']['id']; ?></td>
+                <td><?php echo $item['product']['title']; ?></td>
+                <td><?php echo $item['qty']; ?></td>
+                <td><?php echo money_format('$%i',$item['product']['price']); ?></td>
+                <td><?php echo money_format('$%i', ($item['product']['price'] * $item['qty'])); ?></td>
+                <td><a href="<?php echo CHtml::normalizeUrl(array('cart/remove', 'product' => $item['product']['id']));?>">X</a</td>
+            </tr>
+        <?php endforeach; ?>
+        <tr>
+            <td>
+            </td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Subtotal:</td>
+            <td><?php echo money_format('$%i', $cart->subtotal); ?></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>Tax:</td>
+            <td><?php echo money_format('$%i', $cart->tax); ?></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>shipping:</td>
+            <td><?php echo money_format('$%i', $cart->shipping); ?></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><strong>Total Balance:</strong></td>
+            <td><?php echo money_format('$%i', $cart->total); ?></td>
+        </tr>
+    </table>
+</div>
