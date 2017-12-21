205121278b42750a86192c07d6672de116cf9987
Davey Contreras <contreras.davey@gmail.com>
2017-12-21 09:33:35 -0500

    [crud] - Create CRUD operations for Inventory.

---
+++ protected/views/inventory/_view.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this InventoryController */
+/* @var $data Inventory */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('product_id')); ?>:</b>
+	<?php echo CHtml::encode($data->product_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('qty')); ?>:</b>
+	<?php echo CHtml::encode($data->qty); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('updated')); ?>:</b>
+	<?php echo CHtml::encode($data->updated); ?>
+	<br />
+
+
+</div>
