86cd558890afb409ac50c99660213e6e5aab6490
Davey Contreras <contreras.davey@gmail.com>
2017-12-22 15:15:49 -0500

    [refactor] - Set default action.

--- protected/controllers/OrderController.php
+++ protected/controllers/OrderController.php
@@ -2,6 +2,10 @@
 
 class OrderController extends Controller {
     /**
+     * Default view for DlMachine is Admin View, Index does not exist
+     */
+    public $defaultAction = 'view';
+    /**
      * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
      * using two-column layout. See 'protected/views/layouts/column2.php'.
      */
